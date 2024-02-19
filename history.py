ef398e44b835d7fb8300c94c69b09831b9171860
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-02-19 21:36:55 +0700

    fix: add docstrings but not MVC design pattern

--- history.py
+++ history.py
@@ -1,9 +1,17 @@
+"""This is for the History windows display in the CalculatorUI"""
 import tkinter as tk
 from keypad import Keypad
 
 
 class HistoryWindow(tk.Toplevel):
+    """Tkinter class for displaying a separated window
+    from the parent window"""
     def __init__(self, parent, history_list: list):
+        """
+        Initialize the program and receive value from the parent.
+        :param parent: Parent object to set value to.
+        :param history_list: List from a parent object.
+        """
         super().__init__(parent)
         self.title("History reopen to refresh")
         self.attributes('-topmost', True)
@@ -21,6 +29,7 @@ class HistoryWindow(tk.Toplevel):
         self.init_components()
 
     def init_components(self):
+        """Initialize all the component use in this object window"""
         button = Keypad(self, ['equation', 'result'], 2)
         button.bind('<Button-1>', self.get_value)
 
@@ -30,6 +39,7 @@ class HistoryWindow(tk.Toplevel):
         button.pack(side=tk.TOP, expand=True, fill='both')
 
     def set_equation(self, event):
+        """Set the equation and result value to be ready to return"""
         try:
             # Get the index of the selected item
             selected_index = self.listbox.curselection()
@@ -41,6 +51,8 @@ class HistoryWindow(tk.Toplevel):
             pass
 
     def get_value(self, event):
+        """Set value of the parent display to equation or value
+        depends on the selected button"""
         if event.widget['text'] == 'equation':
             self.parent.display_text.set(self.equation.strip())
             self.parent.calculate_list = [self.equation.strip()]
